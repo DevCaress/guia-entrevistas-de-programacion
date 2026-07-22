995e83d2614aa2a93ade952c687ee11823df80b9
DevCaress <33403609+DevCaress@users.noreply.github.com>
2026-07-21 23:09:16 -0600

    Chore/configure custom domain subpath (#13)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+  site: "https://caress.dev",
+  base: "/technical-interview-guide",
   devToolbar: {
     enabled: false
   },
